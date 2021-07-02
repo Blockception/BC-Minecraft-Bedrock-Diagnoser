70247c1bf99c95c691d8ad6f8bd7f965ccc615e6
DaanV2 <daanverstraten@hotmail.com>
2021-07-02 20:13:25 +0200

    Updated

--- src/Lib/Diagnostics/BehaviorPack/BehaviorPack.ts
+++ src/Lib/Diagnostics/BehaviorPack/BehaviorPack.ts
@@ -1,6 +1,39 @@
 import { TextDocument } from "bc-minecraft-bedrock-project";
+import { FileType } from "bc-minecraft-bedrock-project/lib/src/Lib/Project/BehaviorPack/include";
 import { DiagnosticsBuilder } from "../../Types/DiagnosticsBuilder/DiagnosticsBuilder";
 
 export namespace BehaviorPack {
-  export function Process(doc: TextDocument, diagnoser: DiagnosticsBuilder): void {}
+  export function Process(doc: TextDocument, diagnoser: DiagnosticsBuilder): void {
+    const Type = FileType.detect(doc.uri);
+
+    switch (Type) {
+      case FileType.animation:
+        break;
+      case FileType.animation_controller:
+        break;
+      case FileType.block:
+        break;
+      case FileType.entity:
+        break;
+      case FileType.function:
+        break;
+      case FileType.item:
+        break;
+      case FileType.loot_table:
+        break;
+      case FileType.manifest:
+        break;
+      case FileType.script:
+        break;
+      case FileType.spawn_rule:
+        break;
+      case FileType.structure:
+        break;
+      case FileType.trading:
+        break;
+      default:
+      case FileType.unknown:
+        break;
+    }
+  }
 }
--- src/Lib/Diagnostics/ResourcePack/ResourcePack.ts
+++ src/Lib/Diagnostics/ResourcePack/ResourcePack.ts
@@ -1,6 +1,57 @@
 import { TextDocument } from "bc-minecraft-bedrock-project";
+import { FileType } from "bc-minecraft-bedrock-project/lib/src/Lib/Project/ResourcePack/include";
 import { DiagnosticsBuilder } from "../../Types/DiagnosticsBuilder/DiagnosticsBuilder";
 
 export namespace ResourcePack {
-  export function Process(doc: TextDocument, diagnoser: DiagnosticsBuilder): void {}
+  export function Process(doc: TextDocument, diagnoser: DiagnosticsBuilder): void {
+    const Type = FileType.detect(doc.uri);
+
+    switch (Type) {
+      case FileType.animation:
+        break;
+      case FileType.animation_controller:
+        break;
+      case FileType.attachable:
+        break;
+      case FileType.biomes_client:
+        break;
+      case FileType.block:
+        break;
+      case FileType.entity:
+        break;
+      case FileType.fog:
+        break;
+      case FileType.item:
+        break;
+      case FileType.manifest:
+        break;
+      case FileType.material:
+        break;
+      case FileType.model:
+        break;
+      case FileType.music_definitions:
+        break;
+      case FileType.particle:
+        break;
+      case FileType.render_controller:
+        break;
+      case FileType.sounds:
+        break;
+      case FileType.sounds_definitions:
+        break;
+      case FileType.spawn_rule:
+        break;
+      case FileType.texture:
+        break;
+      case FileType.texture_flipbook_atlas:
+        break;
+      case FileType.texture_item_atlas:
+        break;
+      case FileType.texture_terrain_atlas:
+        break;
+      default:
+      case FileType.unknown:
+        break;
+    }
+  }
 }
