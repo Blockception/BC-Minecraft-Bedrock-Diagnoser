9b2d937d5bf432173b30381cb09f990c45cbbeb1
DaanV2 <daanverstraten@hotmail.com>
2021-07-05 18:04:46 +0200

    Updated

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+export { Diagnoser } from "./Lib/Types/Diagnoser/Diagnoser";
+export { DiagnoserContextCarrier } from "./Lib/Types/Diagnoser/ContextCarrier";
+export { DiagnoserContext, InternalDiagnosticsBuilder } from "./Lib/Types/Diagnoser/DiagnoserContext";
+export { DiagnosticsBuilder, DiagnosticsBuilderContent } from "./Lib/Types/DiagnosticsBuilder/DiagnosticsBuilder";
+export { DiagnosticSeverity } from "./Lib/Types/DiagnosticsBuilder/Severity";
