bd83e2032fabc3e95b1a6ddceb6bbf9ce6c48182
DaanV2 <daanverstraten@hotmail.com>
2021-08-16 19:09:46 +0200

    Updated

--- src/Lib/Diagnostics/Minecraft/Commands.ts
+++ src/Lib/Diagnostics/Minecraft/Commands.ts
@@ -1,4 +1,4 @@
-import { Data } from "bc-minecraft-bedrock-command";
+import { hasCommandData } from "bc-minecraft-bedrock-command";
 import { DiagnosticSeverity } from "../../../main";
 import { DiagnosticsBuilder } from "../../Types/DiagnosticsBuilder/DiagnosticsBuilder";
 import { OffsetWord } from "../../Types/OffsetWord";
@@ -9,7 +9,7 @@ import { OffsetWord } from "../../Types/OffsetWord";
  * @param diagnoser
  */
 export function minecraft_check_command(command: OffsetWord, diagnoser: DiagnosticsBuilder, edu: boolean): void {
-  if (Data.hasCommandData(command.text, edu)) return;
+  if (hasCommandData(command.text, edu)) return;
 
   diagnoser.Add(command.offset, "Command does not exist: " + command.text, DiagnosticSeverity.error, "command.invalid");
 }
