5a63fb53b13d89a6d4d4ae1c818461bebb6fe25c
DaanV2 <daanverstraten@hotmail.com>
2021-08-18 14:33:08 +0200

    Fixed XP

--- src/Lib/Diagnostics/Minecraft/Xp.ts
+++ src/Lib/Diagnostics/Minecraft/Xp.ts
@@ -1,9 +1,9 @@
-import { General } from "bc-minecraft-bedrock-types";
+import { Minecraft } from "bc-minecraft-bedrock-types";
 import { DiagnosticsBuilder, DiagnosticSeverity } from "../../../main";
 import { OffsetWord } from "../../Types/OffsetWord";
 
 export function minecraft_xp_diagnose(value: OffsetWord, diagnoser: DiagnosticsBuilder): void {
-  if (General.XP.is(value.text)) return;
+  if (Minecraft.XP.is(value.text)) return;
 
   diagnoser.Add(value.offset, "Invalid xp value: " + value.text, DiagnosticSeverity.error, "minecraft.xp.invalid");
 }
