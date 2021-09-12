256ff749432e0fff21ea01c84fa6d9f28300e5d6
DaanV2 <daanverstraten@hotmail.com>
2021-09-12 02:06:20 +0200

    Added more todo

--- TODO/Modes/Diagnose.ts
+++ TODO/Modes/Diagnose.ts
@@ -1,19 +1,18 @@
-import { DiagnosticSeverity } from "vscode-languageserver";
 import { LocationWord } from "bc-vscode-words";
-import { ModeCollection } from "./Interface";
-import { DiagnosticsBuilder } from "../../../Diagnostics/Builder";
+import { DiagnosticsBuilder } from "../../Diagnostics/Builder";
+import { ModeCollection } from "bc-minecraft-bedrock-types/lib/src/Modes/ModeCollection";
 
 export function DiagnoseMode(Word: LocationWord, Mode: ModeCollection, builder: DiagnosticsBuilder): void {
   const Text = Word.text;
 
-  const Modes = Mode.Modes;
+  const Modes = Mode.modes;
   for (let I = 0; I < Modes.length; I++) {
     let Element = Modes[I];
 
-    if (Text === Element.Name) {
+    if (Text === Element.name) {
       return;
     }
   }
 
-  builder.AddWord(Word, `Unknown mode type: ${Text} for mode type: '${Mode.Name}'`).code = `${Mode.Name.toLowerCase()}.invalid`;
+  builder.AddWord(Word, `Unknown mode type: ${Text} for mode type: '${Mode.name}'`).code = `${Mode.name.toLowerCase()}.invalid`;
 }
