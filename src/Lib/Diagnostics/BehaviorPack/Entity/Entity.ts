2ea8608782089115abde7f98cdcf525c59cc2b6d
DaanV2 <daanverstraten@hotmail.com>
2021-09-14 23:10:14 +0200

    minecraf:entity -> minecraft:entity

--- src/Lib/Diagnostics/BehaviorPack/Entity/Entity.ts
+++ src/Lib/Diagnostics/BehaviorPack/Entity/Entity.ts
@@ -3,13 +3,13 @@ import { Internal, Map } from "bc-minecraft-bedrock-project";
 export function getUsedComponents(entity: Internal.BehaviorPack.Entity): string[] {
   const out: string[] = [];
 
-  if (entity["minecraf:entity"].components) {
-    Object.getOwnPropertyNames(entity["minecraf:entity"].components).forEach((c) => {
+  if (entity["minecraft:entity"].components) {
+    Object.getOwnPropertyNames(entity["minecraft:entity"].components).forEach((c) => {
       if (!out.includes(c)) out.push(c);
     });
   }
 
-  const groups = entity["minecraf:entity"].component_groups;
+  const groups = entity["minecraft:entity"].component_groups;
 
   if (groups) {
     Map.forEach(groups, (group) => {
@@ -23,7 +23,7 @@ export function getUsedComponents(entity: Internal.BehaviorPack.Entity): string[
 }
 
 export function getUsedGroups(entity: Internal.BehaviorPack.Entity): string[] {
-  const groups = entity["minecraf:entity"].component_groups;
+  const groups = entity["minecraft:entity"].component_groups;
 
   if (groups) {
     return Object.getOwnPropertyNames(groups);
@@ -39,8 +39,8 @@ export function getUsedGroups(entity: Internal.BehaviorPack.Entity): string[] {
  * @returns
  */
 export function hasGroup(entity: Internal.BehaviorPack.Entity, group: string): boolean {
-  if (entity["minecraf:entity"].component_groups) {
-    if (entity["minecraf:entity"].component_groups[group]) {
+  if (entity["minecraft:entity"].component_groups) {
+    if (entity["minecraft:entity"].component_groups[group]) {
       return true;
     }
   }
