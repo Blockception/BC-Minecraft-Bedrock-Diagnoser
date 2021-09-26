01bfe561c0c6d766414340da090ca941215002d8
DaanV2 <daanverstraten@hotmail.com>
2021-09-26 21:00:43 +0200

    Added mcfunctions diagnostics

--- src/Lib/Diagnostics/BehaviorPack/Mcfunction/diagnose.ts
+++ src/Lib/Diagnostics/BehaviorPack/Mcfunction/diagnose.ts
@@ -1,3 +1,14 @@
-export function behaviorpack_functions_diagnose(data: any, builder: any) {
-  //TODO
+import { Text } from 'bc-minecraft-bedrock-project';
+import { DiagnosticsBuilder, DiagnosticSeverity } from '../../../Types/DiagnosticsBuilder/include';
+import { OffsetWord } from '../../../Types/OffsetWord';
+
+export function behaviorpack_functions_diagnose(value: OffsetWord, diagnoser: DiagnosticsBuilder) : boolean {
+  const data = diagnoser.context.getCache();
+  const id = Text.UnQuote(value.text);
+
+  //If project has function then ignore
+  if (data.BehaviorPacks.functions.has(id)) return true;
+
+  diagnoser.Add(value.offset, `Cannot find mcfunction: ${id}`, DiagnosticSeverity.error, "behaviorpack.function.missing");
+  return false;
 }
