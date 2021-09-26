d4d5527c86d9e5d4cf85f4f9ccdb588df02143d5
DaanV2 <daanverstraten@hotmail.com>
2021-09-26 21:00:32 +0200

    Refactor commands ddiagnostics

--- src/Lib/Diagnostics/BehaviorPack/Mcfunction/commands.ts
+++ src/Lib/Diagnostics/BehaviorPack/Mcfunction/commands.ts
@@ -1,8 +1,7 @@
 import { Command, Parameter, ParameterType } from "bc-minecraft-bedrock-command";
 import { ParameterInfo } from "bc-minecraft-bedrock-command/lib/src/Lib/Data/CommandInfo";
 import { TextDocument } from "bc-minecraft-bedrock-project";
-import { DiagnosticsBuilder } from "../../../../main";
-import { DiagnosticSeverity } from "../../../Types/DiagnosticsBuilder/Severity";
+import { DiagnosticSeverity, DiagnosticsBuilder } from "../../../Types/DiagnosticsBuilder/include";
 import { education_enabled } from "../../Definitions";
 import { general_boolean_diagnose } from "../../General/Boolean";
 import { general_float_diagnose } from "../../General/Float";
@@ -16,12 +15,14 @@ import { minecraft_jsonrawtext_diagnose } from "../../Minecraft/JsonRawText";
 import { minecraft_objectives_diagnose } from "../../Minecraft/Objective";
 import { minecraft_selector_diagnose } from "../../Minecraft/Selector";
 import { minecraft_tag_diagnose } from "../../Minecraft/Tag";
+import { behaviorpack_structure_diagnose } from '../Structure/diagnose';
 import { behaviorpack_check_blockdescriptor, behaviorpack_check_blockstates } from "../Block/diagnose";
 import { behaviorpack_entityid_diagnose, behaviorpack_entity_event_diagnose } from "../Entity/diagnose";
 import { behaviorpack_functions_diagnose } from "./diagnose";
 import {
   mode_camerashake_diagnose,
   mode_clone_diagnose,
+  mode_difficulty_diagnose,
   mode_fill_diagnose,
   mode_gamemode_diagnose,
   mode_locatefeature_diagnose,
@@ -31,6 +32,7 @@ import {
   mode_oldblock_diagnose,
   mode_operation_diagnose,
   mode_replace_diagnose,
+  mode_ridefill_diagnose,
   mode_riderules_diagnose,
   mode_rotation_diagnose,
   mode_save_diagnose,
@@ -46,6 +48,8 @@ import { general_keyword_diagnose } from "../../General/Keyword";
 import { minecraft_tickingarea_diagnose } from "../../Minecraft/Tickingarea";
 import { resourcepack_particle_diagnose } from "../../ResourcePack/Particle/diagnose";
 import { resourcepack_sound_diagnose } from "../../ResourcePack/Sounds Definitions/diagnose";
+import { animation_or_controller_diagnose } from '../../ResourcePack/anim or controller';
+import { OffsetWord } from '../../../Types/OffsetWord';
 
 /**
  *
@@ -77,27 +81,40 @@ export function mcfunction_commandscheck(doc: TextDocument, diagnoser: Diagnosti
   }
 }
 
-export function json_commandscheck(prop : string | string[], doc : TextDocument, diagnoser: DiagnosticsBuilder) : void {
+/**
+ * 
+ * @param prop 
+ * @param doc 
+ * @param diagnoser 
+ */
+export function json_commandscheck(prop: string | string[], doc: TextDocument, diagnoser: DiagnosticsBuilder): void {
   if (typeof prop === "string") {
     if (prop.startsWith('/')) {
       commandscheck(prop.substring(1), doc, diagnoser);
     }
   }
   else {
-    prop.forEach(p=>{
+    prop.forEach(p => {
       if (p.startsWith('/')) {
         commandscheck(p.substring(1), doc, diagnoser);
       }
     });
-  }  
+  }
 }
 
-export function commandscheck(commandtext : string, doc : TextDocument, diagnoser: DiagnosticsBuilder) : void {
+/**
+ * 
+ * @param commandtext 
+ * @param doc 
+ * @param diagnoser 
+ * @returns 
+ */
+export function commandscheck(commandtext: string, doc: TextDocument, diagnoser: DiagnosticsBuilder): void {
   if (commandtext.length < 3) return;
 
   const edu = education_enabled(diagnoser);
-  const offset = doc.getText().indexOf(commandtext);  
-  let comm : Command | undefined = Command.parse(commandtext, offset);  
+  const offset = doc.getText().indexOf(commandtext);
+  let comm: Command | undefined = Command.parse(commandtext, offset);
 
   if (comm.isEmpty()) return;
 
@@ -131,16 +148,68 @@ function mcfunction_commandcheck(command: Command, diagnoser: DiagnosticsBuilder
   }
 }
 
+/**Switch data*/
+const ParameterDiagnostics: { [key: number]: (value: OffsetWord, diagnoser: DiagnosticsBuilder) => void | boolean } = {
+  [ParameterType.animation]: animation_or_controller_diagnose,
+  [ParameterType.block]:behaviorpack_check_blockdescriptor,
+  [ParameterType.blockStates]:behaviorpack_check_blockstates,
+  [ParameterType.boolean]:general_boolean_diagnose,
+  [ParameterType.cameraShakeType]:mode_camerashake_diagnose,
+  [ParameterType.cloneMode]:mode_clone_diagnose,
+  //Custom call [ParameterType.command]:,
+  [ParameterType.coordinate]:minecraft_coordinate_diagnose,
+  [ParameterType.difficulty]:mode_difficulty_diagnose,
+  [ParameterType.effect]:minecraft_effect_diagnose,
+  [ParameterType.entity]:behaviorpack_entityid_diagnose,
+  //Custom call [ParameterType.event]:behaviorpack_entity_event_diagnose,
+  [ParameterType.fillMode]:mode_fill_diagnose,
+  [ParameterType.function]:behaviorpack_functions_diagnose,
+  [ParameterType.float]:general_float_diagnose,
+  [ParameterType.gamemode]:mode_gamemode_diagnose,
+  [ParameterType.integer]:general_integer_diagnose,
+  [ParameterType.item]:behaviorpack_item_diagnose,
+  [ParameterType.jsonItem]:minecraft_jsonitem_diagnose,
+  [ParameterType.jsonRawText]:minecraft_jsonrawtext_diagnose,
+  //Custom call [ParameterType.keyword]:general_keyword_diagnose,
+  [ParameterType.locateFeature]:mode_locatefeature_diagnose,
+  //Custom call [ParameterType.message]:,
+  [ParameterType.maskMode]:mode_mask_diagnose,
+  [ParameterType.mirror]:mode_mirror_diagnose,
+  [ParameterType.musicRepeatMode]:mode_musicrepeat_diagnose,
+  [ParameterType.objective]:minecraft_objectives_diagnose,
+  [ParameterType.oldBlockMode]:mode_oldblock_diagnose,
+  [ParameterType.operation]:mode_operation_diagnose,
+  [ParameterType.particle]:resourcepack_particle_diagnose,
+  [ParameterType.replaceMode]:mode_replace_diagnose,
+  [ParameterType.rideRules]:mode_riderules_diagnose,
+  [ParameterType.ridefillMode]:mode_ridefill_diagnose,
+  [ParameterType.rotation]:mode_rotation_diagnose,
+  [ParameterType.saveMode]:mode_save_diagnose,
+  //Custom call [ParameterType.selector]:minecraft_selector_diagnose,
+  [ParameterType.slotType]:mode_slottype_diagnose,
+  //Custom call [ParameterType.slotID]:,
+  [ParameterType.sound]:resourcepack_sound_diagnose,
+  [ParameterType.string]:general_string_diagnose,
+  [ParameterType.structure]:behaviorpack_structure_diagnose,
+  [ParameterType.structureAnimationMode]:mode_structureanimation_diagnose,
+  [ParameterType.tag]:minecraft_tag_diagnose,
+  [ParameterType.teleportRules]:mode_teleportrules_diagnose,
+  [ParameterType.tickingarea]:minecraft_tickingarea_diagnose,
+  [ParameterType.time]:mode_time_diagnose,
+  //Custom call ParameterType.unknown]:(item, diagnoser)=>diagnoser.Add(item.offset, "Unknown parametype: " + item.type, DiagnosticSeverity.warning, "debugger.error"),
+  [ParameterType.xp]:minecraft_xp_diagnose,
+}
+
 /**
  *
  * @param pattern
  * @param data
- * @param builder
+ * @param diagnoser
  * @param Com
  * @param edu
  * @returns
  */
-function mcfunction_diagnoseparameter(pattern: ParameterInfo, data: Parameter, builder: DiagnosticsBuilder, Com: Command, edu: boolean): void | boolean {
+function mcfunction_diagnoseparameter(pattern: ParameterInfo, data: Parameter, diagnoser: DiagnosticsBuilder, Com: Command, edu: boolean): void | boolean {
   if (pattern === undefined || data === undefined) return;
 
   if (pattern.options) {
@@ -155,93 +224,29 @@ function mcfunction_diagnoseparameter(pattern: ParameterInfo, data: Parameter, b
     }
   }
 
+  const call = ParameterDiagnostics[pattern.type];
+
+  if (call) {
+    return call(data, diagnoser);
+  }
+
+  //Custom calls
   switch (pattern.type) {
-    case ParameterType.block:
-      return behaviorpack_check_blockdescriptor(data, builder);
-    case ParameterType.blockStates:
-      return behaviorpack_check_blockstates(data, builder);
-    case ParameterType.boolean:
-      return general_boolean_diagnose(data, builder);
-    case ParameterType.cameraShakeType:
-      return mode_camerashake_diagnose(data, builder);
-    case ParameterType.cloneMode:
-      return mode_clone_diagnose(data, builder);
     case ParameterType.command:
-      return minecraft_check_command(data, builder, edu);
-    case ParameterType.coordinate:
-      return minecraft_coordinate_diagnose(data, builder);
-    case ParameterType.effect:
-      return minecraft_effect_diagnose(data, builder);
-    case ParameterType.entity:
-      return behaviorpack_entityid_diagnose(data, builder);
+      return minecraft_check_command(data, diagnoser, edu);
     case ParameterType.event:
-      return behaviorpack_entity_event_diagnose(data, builder, Com);
-    case ParameterType.fillMode:
-      return mode_fill_diagnose(data, builder);
-    case ParameterType.float:
-      return general_float_diagnose(data, builder);
-    case ParameterType.function:
-      return behaviorpack_functions_diagnose(data, builder);
-    case ParameterType.gamemode:
-      return mode_gamemode_diagnose(data, builder);
-    case ParameterType.integer:
-      return general_integer_diagnose(data, builder);
-    case ParameterType.item:
-      return behaviorpack_item_diagnose(data, builder);
-    case ParameterType.jsonItem:
-      return minecraft_jsonitem_diagnose(data, builder);
-    case ParameterType.jsonRawText:
-      return minecraft_jsonrawtext_diagnose(data, builder);
+      return behaviorpack_entity_event_diagnose(data, diagnoser, Com);      
     case ParameterType.keyword:
-      return general_keyword_diagnose(pattern.text, data, builder);
-    case ParameterType.locateFeature:
-      return mode_locatefeature_diagnose(data, builder);
-    case ParameterType.maskMode:
-      return mode_mask_diagnose(data, builder);
-    case ParameterType.mirror:
-      return mode_mirror_diagnose(data, builder);
-    case ParameterType.musicRepeatMode:
-      return mode_musicrepeat_diagnose(data, builder);
-    case ParameterType.oldBlockMode:
-      return mode_oldblock_diagnose(data, builder);
-    case ParameterType.objective:
-      return minecraft_objectives_diagnose(data, builder);
-    case ParameterType.operation:
-      return mode_operation_diagnose(data, builder);
-    case ParameterType.particle:
-      return resourcepack_particle_diagnose(data, builder);
-    case ParameterType.replaceMode:
-      return mode_replace_diagnose(data, builder);
-    case ParameterType.rideRules:
-      return mode_riderules_diagnose(data, builder);
-    case ParameterType.rotation:
-      return mode_rotation_diagnose(data, builder);
-    case ParameterType.saveMode:
-      return mode_save_diagnose(data, builder);
-    case ParameterType.selector:
-      return minecraft_selector_diagnose(pattern, data, builder);
+      return general_keyword_diagnose(pattern.text, data, diagnoser);
+    case ParameterType.message:
+      return true; //TODO message check
+      case ParameterType.selector:
+      return minecraft_selector_diagnose(pattern, data, diagnoser);
     case ParameterType.slotID:
-      return mode_slotid_diagnose(data, Com, builder);
-    case ParameterType.slotType:
-      return mode_slottype_diagnose(data, builder);
-    case ParameterType.sound:
-      return resourcepack_sound_diagnose(data, builder);
-    case ParameterType.string:
-      return general_string_diagnose(data, builder);
-    case ParameterType.structureAnimationMode:
-      return mode_structureanimation_diagnose(data, builder);
-    case ParameterType.tag:
-      return minecraft_tag_diagnose(data, builder);
-    case ParameterType.teleportRules:
-      return mode_teleportrules_diagnose(data, builder);
-    case ParameterType.tickingarea:
-      return minecraft_tickingarea_diagnose(data, builder);
-    case ParameterType.time:
-        return mode_time_diagnose(data, builder);
-    case ParameterType.xp:
-      return minecraft_xp_diagnose(data, builder);
+      return mode_slotid_diagnose(data, Com, diagnoser);
+      
     case ParameterType.unknown:
-      builder.Add(data.offset, "Unknown parametype: " + pattern.type, DiagnosticSeverity.warning, "debugger.error");
-      return;
+      diagnoser.Add(data.offset, `Unknown parametype: ${pattern.type}:${ParameterType[pattern.type]}`, DiagnosticSeverity.warning, "debugger.error");
+      return false;
   }
 }
