9e248ab7a874489745d09a902be28ca002b0f4cf
DaanV2 <daanverstraten@hotmail.com>
2021-09-26 20:59:59 +0200

    Added item diagnostics

--- src/Lib/Diagnostics/BehaviorPack/Item/diagnose.ts
+++ src/Lib/Diagnostics/BehaviorPack/Item/diagnose.ts
@@ -1,6 +1,19 @@
-import { DiagnosticsBuilder } from "../../../../main";
-import { OffsetWord } from "../../../Types/OffsetWord";
 
-export function behaviorpack_item_diagnose(data: OffsetWord, diagnoser: DiagnosticsBuilder): void {
-  //TODO
+import { DiagnosticsBuilder, DiagnosticSeverity } from '../../../Types/DiagnosticsBuilder/include';
+import { OffsetWord } from "../../../Types/OffsetWord";
+import { check_definition_value } from '../../Definitions';
+
+export function behaviorpack_item_diagnose(value: OffsetWord, diagnoser: DiagnosticsBuilder): boolean {
+  const id = value.text;
+  //Defined in McProject
+  if (check_definition_value(diagnoser.project.definitions.item, id, diagnoser)) return true;
+
+  const data = diagnoser.context.getCache();
+
+  //Project has item
+  if (data.BehaviorPacks.items.has(id)) return true;
+
+  //Nothing then report error
+  diagnoser.Add(`"${id}"`, `Cannot find behaviorpack item definition: ${id}`, DiagnosticSeverity.error, "behaviorpack.item.missing");
+  return false;
 }
