c77051bae02fcc77ebdca459cdc0eae8b1af9a68
DaanV2 <daanverstraten@hotmail.com>
2021-09-28 16:26:16 +0200

    Fixed entity check

--- src/Lib/Diagnostics/BehaviorPack/Entity/diagnose.ts
+++ src/Lib/Diagnostics/BehaviorPack/Entity/diagnose.ts
@@ -11,7 +11,7 @@ import { check_definition_value, education_enabled } from "../../Definitions";
  * @returns
  */
 export function behaviorpack_entityid_diagnose(value: Types.OffsetWord, diagnoser: DiagnosticsBuilder): void {
-  const id = value.text;
+  let id = value.text;
 
   //Defined in McProject
   if (check_definition_value(diagnoser.project.definitions.entity, id, diagnoser)) return;
@@ -26,6 +26,24 @@ export function behaviorpack_entityid_diagnose(value: Types.OffsetWord, diagnose
   //Vanilla has entity
   if (MinecraftData.BehaviorPack.hasEntity(id, edu)) return;
 
+  //No namespace?
+  if (!id.includes(':')) {
+    id = "minecraft:" + id;
+
+    //Defined in McProject
+    if (check_definition_value(diagnoser.project.definitions.entity, id, diagnoser)) return;
+
+    const data = diagnoser.context.getCache();
+
+    //Project has entity
+    if (data.hasEntity(id)) return;
+
+    const edu = education_enabled(diagnoser);
+
+    //Vanilla has entity
+    if (MinecraftData.BehaviorPack.hasEntity(id, edu)) return;
+  }
+
   //Nothing then report error
   diagnoser.Add(value.offset, `Cannot find entity definition: ${id}`, DiagnosticSeverity.error, "behaviorpack.entity.missing");
 }
@@ -35,10 +53,10 @@ export function behaviorpack_entityid_diagnose(value: Types.OffsetWord, diagnose
  * @param diagnoser
  * @returns
  */
- export function behaviorpack_entity_spawnegg_diagnose(value: Types.OffsetWord, diagnoser: DiagnosticsBuilder): void {
+export function behaviorpack_entity_spawnegg_diagnose(value: Types.OffsetWord, diagnoser: DiagnosticsBuilder): void {
   const id = value.text.replace('_spawn_egg', "");
 
-  behaviorpack_entityid_diagnose({offset:value.offset,text:id}, diagnoser);
+  behaviorpack_entityid_diagnose({ offset: value.offset, text: id }, diagnoser);
 }
 
 /**Checks if the event is defined on the correct entities
@@ -46,14 +64,14 @@ export function behaviorpack_entityid_diagnose(value: Types.OffsetWord, diagnose
  * @param builder
  * @param Com
  */
-export function behaviorpack_entity_event_diagnose(data: Types.OffsetWord, diagnoser: DiagnosticsBuilder, Com: Command): void {  
+export function behaviorpack_entity_event_diagnose(data: Types.OffsetWord, diagnoser: DiagnosticsBuilder, Com: Command): void {
   const edu = education_enabled(diagnoser);
   const matches = Com.getBestMatch(edu);
 
-  if(matches.length !== 1) return;
+  if (matches.length !== 1) return;
 
-  const entityid_index = matches[0].parameters.findIndex(p=>{p.type === ParameterType.entity});
-  let entityid : string | undefined= undefined;
+  const entityid_index = matches[0].parameters.findIndex(p => { p.type === ParameterType.entity });
+  let entityid: string | undefined = undefined;
 
   if (entityid_index >= 0) {
     entityid = Com.parameters[entityid_index]?.text;
