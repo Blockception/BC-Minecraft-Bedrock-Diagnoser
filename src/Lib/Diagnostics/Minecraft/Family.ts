c235eef683f8d026cecc64746562b6a19727c76e
DaanV2 <daanverstraten@hotmail.com>
2021-09-28 16:00:39 +0200

    Added family diagnostics

---
+++ src/Lib/Diagnostics/Minecraft/Family.ts
@@ -0,0 +1,28 @@
+import { DiagnosticsBuilder, DiagnosticSeverity } from "../../../Lib/Types/DiagnosticsBuilder/include";
+import { Types } from "bc-minecraft-bedrock-types";
+import { Text } from 'bc-minecraft-bedrock-project';
+import { check_definition_value } from "../Definitions";
+import { MinecraftData } from 'bc-minecraft-bedrock-vanilla-data';
+
+export function minecraft_family_diagnose(value: Types.OffsetWord, diagnoser: DiagnosticsBuilder): boolean {
+
+  const id = Text.UnQuote(value.text);
+  
+  //Defined in McProject
+  if (check_definition_value(diagnoser.project.definitions.family, id, diagnoser)) return true;
+
+  let out = false;
+  
+  const data = diagnoser.context.getCache();
+  //Project has defined
+  data.BehaviorPacks.entities.forEach(entity=>{if (entity.families.includes(id)) out = true;});
+
+  if (out) return true;
+
+  //Vanilla has defined
+  if (MinecraftData.General.Entities.families.includes(id)) return true;  
+
+  //Nothing then report error
+  diagnoser.Add(value, `Cannot find family definition: ${id}`, DiagnosticSeverity.error, "minecraft.family.missing");
+  return false;
+}
