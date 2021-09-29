84107bf59146c1cf1ea0d56eefa0d06ff4afba6c
DaanV2 <daanverstraten@hotmail.com>
2021-09-29 21:29:16 +0200

    Fixed

--- test/Lib/Diagnostics/Molang/implementation.test.ts
+++ test/Lib/Diagnostics/Molang/implementation.test.ts
@@ -15,12 +15,48 @@ describe("Molang", () => {
       using.variables.using.push("spleef", "state");
       using.variables.defined.push("state")
 
-      owner.temps.defined.push("push");
+      owner.temps.defined.push("foo");
       owner.variables.defined.push("spleef");
 
       diagnose_molang_implementation({ id: "animation.example.walk", molang: using }, { id: "minecraft:sheep", molang: owner }, "entity", diganoser);
 
       diganoser.expectEmpty();
+    });
+
+    it("1 error", () => {
+      const diganoser = TestDiagnoser.Create();
+
+      const using = MolangFullSet.create();
+      const owner = MolangFullSet.create();
+
+      using.queries.using.push("is_jumping")
+      using.temps.using.push("foo");
+      using.variables.using.push("spleef", "state");
+      using.variables.defined.push("state")
+
+      owner.variables.defined.push("spleef");
+
+      diagnose_molang_implementation({ id: "animation.example.walk", molang: using }, { id: "minecraft:sheep", molang: owner }, "entity", diganoser);
+
+      diganoser.expectAmount(1);
+    });
+
+    it("1 error", () => {
+      const diganoser = TestDiagnoser.Create();
+
+      const using = MolangFullSet.create();
+      const owner = MolangFullSet.create();
+
+      using.queries.using.push("is_jumping")
+      using.temps.using.push("foo");
+      using.variables.using.push("spleef", "state");
+      using.variables.defined.push("state")
+
+      owner.temps.defined.push("foo");
+
+      diagnose_molang_implementation({ id: "animation.example.walk", molang: using }, { id: "minecraft:sheep", molang: owner }, "entity", diganoser);
+
+      diganoser.expectAmount(1);
     });
   });
 });
