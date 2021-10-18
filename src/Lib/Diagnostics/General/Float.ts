e0eb0c579a06496debede917732b5e39162d018b
DaanV2 <daanverstraten@hotmail.com>
2021-10-18 14:52:37 +0200

    Added float postive diagnostics

--- src/Lib/Diagnostics/General/Float.ts
+++ src/Lib/Diagnostics/General/Float.ts
@@ -9,3 +9,15 @@ export function general_float_diagnose(value: Types.OffsetWord, diagnoser: Diagn
   diagnoser.Add(value, "Invalid float value: " + value.text, DiagnosticSeverity.error, "float.invalid");
   return false;
 }
+
+export function general_positive_float_diagnose(value: Types.OffsetWord, diagnoser: DiagnosticsBuilder): boolean {
+  //If its not a float then skip positive check
+  if (!general_float_diagnose(value, diagnoser)) return false;
+
+  const n = Number.parseInt(value.text);
+
+  if (n >= 0) return true;
+
+  diagnoser.Add(value, `expected a positive float but got: ${n}`, DiagnosticSeverity.error, "float.positive.only");
+  return false;
+}
