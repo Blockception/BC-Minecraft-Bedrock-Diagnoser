772217b8aae0ca8b701a254395be7f4214499049
DaanV2 <daanverstraten@hotmail.com>
2021-10-20 11:52:14 +0200

    Added work pack manifest checks

---
+++ src/Lib/Diagnostics/WorldPack/Manifest/entry.ts
@@ -0,0 +1,15 @@
+import { Internal, TextDocument } from "bc-minecraft-bedrock-project";
+import { DiagnosticsBuilder } from "../../../Types/DiagnosticsBuilder/DiagnosticsBuilder";
+import { Json } from "../../Json/Json";
+import { minecraft_manifest_diagnose } from "../../Minecraft/Manifest";
+
+/**Diagnoses the given document as an bp manifest
+ * @param doc The text document to diagnose
+ * @param diagnoser The diagnoser builder to receive the errors*/
+export function Diagnose(doc: TextDocument, diagnoser: DiagnosticsBuilder): void {
+  const manifest = Json.LoadReport<Internal.Manifest>(doc, diagnoser);
+
+  if (!Json.TypeCheck(manifest, diagnoser, "manifest", "minecraft.manifest.invalid", Internal.Manifest.is)) return;
+
+  minecraft_manifest_diagnose(manifest, diagnoser);
+}
---
+++ src/Lib/Diagnostics/WorldPack/WorldPack.ts
@@ -0,0 +1,20 @@
+import { TextDocument } from "bc-minecraft-bedrock-project";
+import { DiagnosticsBuilder } from "../../Types/DiagnosticsBuilder/DiagnosticsBuilder";
+
+import * as Manifest from "./Manifest/entry";
+
+export namespace WorldPack {
+  /**Processes and diagnoses the given textdocument
+   * @param doc The document to process / diagnose
+   * @param diagnoser The diagnoser to report to
+   * @returns `true` or `false` whenever or not it was succesfull*/
+  export function Process(doc: TextDocument, diagnoser: DiagnosticsBuilder): boolean {
+    //retrieve filter doc type
+    if (doc.uri.endsWith("manifest.json")) {
+      Manifest.Diagnose(doc, diagnoser);
+      return true;
+    }
+
+    return true;
+  }
+}
