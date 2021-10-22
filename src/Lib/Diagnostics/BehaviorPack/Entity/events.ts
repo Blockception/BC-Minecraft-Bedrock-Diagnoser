8c86a0ae9a6049559f20e8338959ce807aa7154c
DaanV2 <daanverstraten@hotmail.com>
2021-10-22 21:13:41 +0200

    Added entity checking

--- src/Lib/Diagnostics/BehaviorPack/Entity/events.ts
+++ src/Lib/Diagnostics/BehaviorPack/Entity/events.ts
@@ -1,13 +1,25 @@
 import { Internal, Map } from "bc-minecraft-bedrock-project";
 import { DiagnosticsBuilder, DiagnosticSeverity } from "../../../../main";
 
+export function behaviorpack_entity_check_events(
+  events: Map<Internal.BehaviorPack.EntityEvent> | Internal.BehaviorPack.EntityEvent[],
+  diagnoser: DiagnosticsBuilder,
+  component_groups?: Map<Internal.BehaviorPack.EntityComponentContainer>
+) {
+  if (Array.isArray(events)) {
+    events.forEach((event) => behaviorpack_entity_check_event(event, diagnoser, component_groups));
+  } else {
+    Map.forEach(events, (event, key) => behaviorpack_entity_check_event(event, diagnoser, component_groups));
+  }
+}
+
 /**
  *
  * @param event
  * @param diagnoser
  * @param component_groups
  */
-export function behaviorpack_entity_check_events(
+export function behaviorpack_entity_check_event(
   event: Internal.BehaviorPack.EntityEvent,
   diagnoser: DiagnosticsBuilder,
   component_groups?: Map<Internal.BehaviorPack.EntityComponentContainer>
@@ -16,11 +28,11 @@ export function behaviorpack_entity_check_events(
   has_groups(diagnoser, event.remove?.component_groups, component_groups);
 
   event.randomize?.forEach((item) => {
-    behaviorpack_entity_check_events(item, diagnoser, component_groups);
+    behaviorpack_entity_check_event(item, diagnoser, component_groups);
   });
 
   event.sequence?.forEach((item) => {
-    behaviorpack_entity_check_events(item, diagnoser, component_groups);
+    behaviorpack_entity_check_event(item, diagnoser, component_groups);
   });
 }
 
