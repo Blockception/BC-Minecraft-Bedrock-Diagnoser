6881407f7e86c9988c6cfaabf9e24396ff55b238
DaanV2 <daanverstraten@hotmail.com>
2021-10-29 13:08:32 +0200

    Updated sound diagnostics

--- src/Lib/Diagnostics/ResourcePack/Sounds/diagnostics.ts
+++ src/Lib/Diagnostics/ResourcePack/Sounds/diagnostics.ts
@@ -1,14 +1,18 @@
 import { Types } from "bc-minecraft-bedrock-types";
+import { MinecraftData } from "bc-minecraft-bedrock-vanilla-data";
 import { DiagnosticsBuilder } from "../../../Types/DiagnosticsBuilder/DiagnosticsBuilder";
 import { DiagnosticSeverity } from "../../../Types/DiagnosticsBuilder/Severity";
+import { education_enabled } from "../../Definitions";
 
 export function diagnose_resourcepack_sounds(data: Types.Definition | undefined, diagnoser: DiagnosticsBuilder): void {
   if (data === undefined) return;
 
   const pdata = diagnoser.context.getCache();
+  const edu = education_enabled(diagnoser);
 
   Types.Definition.forEach(data, (ref, id) => {
     if (pdata.ResourcePacks.sounds.has(id)) return;
+    if (MinecraftData.ResourcePack.hasSound(id, edu)) return;
 
     diagnoser.Add(ref + "/" + id, `Cannot find sound definition: ${id}`, DiagnosticSeverity.error, "resourpack.sound.missing");
   });
