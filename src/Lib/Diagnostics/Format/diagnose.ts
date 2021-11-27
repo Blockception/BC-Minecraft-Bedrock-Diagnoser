7df73a50274811bbb3d76335bb6bea0700446698
DaanV2 <daanverstraten@hotmail.com>
2021-11-27 14:00:50 +0100

    temp disabled path length

--- src/Lib/Diagnostics/Format/diagnose.ts
+++ src/Lib/Diagnostics/Format/diagnose.ts
@@ -1,5 +1,5 @@
-import { Pack } from 'bc-minecraft-bedrock-project';
-import { DiagnosticsBuilder, DiagnosticSeverity } from '../../Types/DiagnosticsBuilder/include';
+import { Pack } from "bc-minecraft-bedrock-project";
+import { DiagnosticsBuilder, DiagnosticSeverity } from "../../Types/DiagnosticsBuilder/include";
 
 /**Max relative length form pack / worldtemplate */
 const LengthFromRoot = 70;
@@ -7,8 +7,10 @@ const SegmentLength = 60;
 const PackNameLength = 10;
 
 export function format_diagnose_path(pack: Pack, uri: string, diagnoser: DiagnosticsBuilder) {
+  //TODO redo
+  /**
   const root = pack.folder;
-  const relpath = uri.replace(pack.folder, "");
+  const relpath = uri.replace(root, "");
 
   if (relpath.length > LengthFromRoot) {
     diagnoser.Add(0, `Path is too long: '${relpath}', should be maximum of: ${LengthFromRoot} but is: ${relpath.length} characters long`, DiagnosticSeverity.error, "minecraft.format.path.length");
@@ -20,6 +22,6 @@ export function format_diagnose_path(pack: Pack, uri: string, diagnoser: Diagnos
       diagnoser.Add(0, `Segment of path is too long: '${seg}' in ${relpath}, should be maximum of: ${SegmentLength} but is: ${seg.length} characters long`, DiagnosticSeverity.error, "minecraft.format.path.length");
     }
   });
-
+   */
   //TODO folder name check
 }
