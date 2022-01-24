43d4c60929f34c55369b9551bc78dd1f58690113
DaanV2 <daanverstraten@hotmail.com>
2022-01-24 11:41:33 +0100

    Checking for correct mcstructure id

--- src/Lib/Diagnostics/BehaviorPack/Structure/diagnose.ts
+++ src/Lib/Diagnostics/BehaviorPack/Structure/diagnose.ts
@@ -5,6 +5,17 @@ import { check_definition_value } from '../../Definitions';
 
 export function behaviorpack_structure_diagnose(value: Types.OffsetWord, diagnoser: DiagnosticsBuilder): boolean {
   const id = value.text;
+
+  //If it has a slash it needs ""
+  if (id.includes('/')) {
+    if (id.startsWith('"') && id.endsWith('"')) {
+
+    }
+    else {
+      diagnoser.Add(value, `A structure id with '/' needs quotes surrounding it: ${id} => "${id}"`, DiagnosticSeverity.error, "behaviorpack.mcstructure.invalid");
+    }
+  }
+
   //Defined in McProject
   if (check_definition_value(diagnoser.project.definitions.structure, id, diagnoser)) return true;
 
