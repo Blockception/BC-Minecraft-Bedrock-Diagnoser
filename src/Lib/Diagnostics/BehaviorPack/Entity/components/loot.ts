f6b046685ffd80c318c491c6bd11ba9170e82b24
DaanV2 <daanverstraten@hotmail.com>
2022-02-16 15:35:52 +0100

    Updated

--- src/Lib/Diagnostics/BehaviorPack/Entity/components/loot.ts
+++ src/Lib/Diagnostics/BehaviorPack/Entity/components/loot.ts
@@ -1,14 +1,18 @@
 import { DiagnosticsBuilder } from "../../../../Types/DiagnosticsBuilder/DiagnosticsBuilder";
-import { Internal, TextDocument } from "bc-minecraft-bedrock-project";
+import { Internal } from "bc-minecraft-bedrock-project";
 import { behaviorpack_loot_table_diagnose } from "../../Loot Table/diagnose";
 
-export function behaviorpack_entity_components_loot(container: Internal.BehaviorPack.EntityComponentContainer, diagnoser: DiagnosticsBuilder) {
-  const loot = container["minecraft:loot"];
+export function behaviorpack_entity_components_loot(
+  container: Internal.BehaviorPack.EntityComponentContainer,
+  diagnoser: DiagnosticsBuilder
+) {
+  checkitem(container["minecraft:loot"], diagnoser);
+  checkitem(container["minecraft:equipment"], diagnoser);
+}
 
-  if (loot === undefined) return;
-  if (typeof loot.table !== "string") return;
-
-  const table = <string>loot.table;
-
+function checkitem( component: { table?: string } | undefined, diagnoser: DiagnosticsBuilder): void {
+  if (component === undefined) return;
+  if (typeof component.table !== "string") return;
+  const table = component.table;
   behaviorpack_loot_table_diagnose(table, diagnoser);
 }
