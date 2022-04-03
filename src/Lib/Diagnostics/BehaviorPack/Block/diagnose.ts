f16309fb549f01db40807b7da418dc94e40a8291
DaanV2 <daanverstraten@hotmail.com>
2022-04-03 21:59:31 +0200

    Adding block diagnostics

--- src/Lib/Diagnostics/BehaviorPack/Block/diagnose.ts
+++ src/Lib/Diagnostics/BehaviorPack/Block/diagnose.ts
@@ -21,20 +21,20 @@ export function behaviorpack_check_blockdescriptor(blockDescriptor: Types.Offset
  * @param diagnoser
  * @returns
  */
-export function behaviorpack_check_blockid(blockDescriptor: Types.OffsetWord, diagnoser: DiagnosticsBuilder): void {
+export function behaviorpack_check_blockid(blockDescriptor: Types.OffsetWord, diagnoser: DiagnosticsBuilder): boolean {
   let id = Minecraft.Block.getId(blockDescriptor.text);
 
   //Defined in McProject
-  if (check_definition_value(diagnoser.project.definitions.block, id, diagnoser)) return;
+  if (check_definition_value(diagnoser.project.definitions.block, id, diagnoser)) return true;
   const data = diagnoser.context.getCache();
 
   //Project has block
-  if (data.hasBlock(id)) return;
+  if (data.hasBlock(id)) return true;
 
   const edu = education_enabled(diagnoser);
 
   //Vanilla has block
-  if (MinecraftData.BehaviorPack.hasBlock(id, edu)) return;
+  if (MinecraftData.BehaviorPack.hasBlock(id, edu)) return true;
 
   //Missing namespace?
   if (!id.includes(":")) {
@@ -42,17 +42,18 @@ export function behaviorpack_check_blockid(blockDescriptor: Types.OffsetWord, di
     id = "minecraft:" + id;
 
     //Defined in McProject
-    if (check_definition_value(diagnoser.project.definitions.block, id, diagnoser)) return;
+    if (check_definition_value(diagnoser.project.definitions.block, id, diagnoser)) return true;
 
     //Project has block
-    if (data.hasBlock(id)) return;
+    if (data.hasBlock(id)) return true;
   }
   
   //Vanilla has block
-  if (MinecraftData.BehaviorPack.hasBlock(id, edu)) return;
+  if (MinecraftData.BehaviorPack.hasBlock(id, edu)) return true;
 
   //Nothing then report error
   diagnoser.Add(id, `Cannot find block definition: ${id}`, DiagnosticSeverity.error, "behaviorpack.block.missing");
+  return false;
 }
 
 /**Checks if the blocks exists in the project or in vanilla, if not then a bug is reported
--- src/Lib/Diagnostics/BehaviorPack/Item/diagnose.ts
+++ src/Lib/Diagnostics/BehaviorPack/Item/diagnose.ts
@@ -2,6 +2,7 @@ import { MinecraftData } from "bc-minecraft-bedrock-vanilla-data";
 import { DiagnosticsBuilder, DiagnosticSeverity } from "../../../Types/DiagnosticsBuilder/include";
 import { Types } from "bc-minecraft-bedrock-types";
 import { check_definition_value, education_enabled } from "../../Definitions";
+import { behaviorpack_check_blockid } from '../Block/diagnose';
 
 interface Item extends Types.OffsetWord {
   data?: number;
@@ -25,12 +26,15 @@ export function behaviorpack_item_diagnose(value: Item, diagnoser: DiagnosticsBu
     //Defined in McProject
     if (check_definition_value(diagnoser.project.definitions.item, id, diagnoser)) return true;
 
-    //Project has block
+    //Project has item
     if (data.hasItem(id)) return true;
   }
 
-  //Vanilla has block
+  //Vanilla has item
   if (MinecraftData.BehaviorPack.hasItem(id, education_enabled(diagnoser))) return true;
+
+  //Is it a block item?
+  if (behaviorpack_check_blockid(value, diagnoser)) return true;
 
   //Nothing then report error
   diagnoser.Add(value, `Cannot find behaviorpack item definition: ${id}`, DiagnosticSeverity.error, "behaviorpack.item.missing");
