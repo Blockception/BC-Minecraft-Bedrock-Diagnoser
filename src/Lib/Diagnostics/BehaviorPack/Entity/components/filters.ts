22b39a577b5f41900938352b2ed20f1be09e6793
DaanV2 <daanverstraten@hotmail.com>
2022-07-25 22:16:45 +0200

    Updated

--- src/Lib/Diagnostics/BehaviorPack/Entity/components/filters.ts
+++ src/Lib/Diagnostics/BehaviorPack/Entity/components/filters.ts
@@ -2,14 +2,16 @@ import { DiagnosticsBuilder } from "../../../../../main";
 import { minecraft_diagnose_filters } from "../../../Minecraft/Filter/diagnose";
 
 export function behaviorpack_entity_components_filters(container: any, diagnoser: DiagnosticsBuilder) {
+  if (typeof container !== "object") return;
+
   traverse_object(container, diagnoser);
 }
 
-function traverse_object(object: any, diagnoser: DiagnosticsBuilder) {
-  const properties = Object.getOwnPropertyNames(object);
+function traverse_object(data: { [key: string]: any }, diagnoser: DiagnosticsBuilder) {
+  const properties = Object.getOwnPropertyNames(data);
 
   properties.forEach((property_key) => {
-    const property = object[property_key];
+    const property = data[property_key];
     if (typeof property === "object") {
       if (property_key.includes("filter")) {
         minecraft_diagnose_filters(property, diagnoser);
