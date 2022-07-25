72138e38d7f2c4a3b9e4f73e4b2acc59de18d95c
DaanV2 <daanverstraten@hotmail.com>
2022-07-25 20:38:59 +0200

    Updated types

---
+++ src/Lib/Diagnostics/BehaviorPack/Entity/components/diagnose.ts
@@ -0,0 +1,8 @@
+import { Internal } from "bc-minecraft-bedrock-project";
+import { DiagnosticsBuilder } from '../../../../Types/DiagnosticsBuilder/include';
+
+
+
+export function diagnose_entity_components(container : Internal.BehaviorPack.Entity, diagnoser : DiagnosticsBuilder) : void {
+    
+}
