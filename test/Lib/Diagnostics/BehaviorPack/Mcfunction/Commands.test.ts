64302dbcb7316f6b08d6d3d9d7c04bd423d310a1
Daan Verstraten <daanverstraten@hotmail.com>
2023-04-27 21:31:02 +0200

    Feature/vanilla update (#126)
    * Updated diagnoser
    * Cleaned
    * Adding better block state diagnostics
    * Removed reporter

---
+++ test/Lib/Diagnostics/BehaviorPack/Mcfunction/Commands.test.ts
@@ -0,0 +1,58 @@
+import { ProjectData, TextDocument } from "bc-minecraft-bedrock-project";
+import { mcfunction_commandsCheck } from "../../../../../src/Lib/Diagnostics/BehaviorPack/Mcfunction";
+import { TestDiagnoser } from "../../../../diagnoser";
+
+describe("BehaviorPack", () => {
+  describe("Mcfunctions", () => {
+    describe("Commands", () => {
+      var diagnoser: TestDiagnoser;
+      var data: ProjectData;
+
+      beforeEach(() => {
+        diagnoser = TestDiagnoser.Create();
+        data = diagnoser.context.getCache();
+      });
+
+      //Correct commands
+      const correctsCommands: string[] = ['setblock ~ ~ ~ spruce_log ["pillar_axis":"x"]'];
+
+      for (let I = 0; I < correctsCommands.length; I++) {
+        const command = correctsCommands[I];
+
+        it(`correct ${command}`, () => {
+          const doc: TextDocument = {
+            uri: "fake",
+            getText(range) {
+              return command;
+            },
+          };
+
+          mcfunction_commandsCheck(doc, diagnoser);
+          diagnoser.expectEmpty();
+        });
+      }
+
+      //Incorrect commands
+      const incorrectCommands: string[] = [
+        "setblock ~ ~ ~ spruce_log [pillar_axis:x]",
+        "setblock ~ ~ ~ stone [pillar_axis:x]"
+      ];
+
+      for (let I = 0; I < incorrectCommands.length; I++) {
+        const command = incorrectCommands[I];
+
+        it(`incorrect ${command}`, () => {
+          const doc: TextDocument = {
+            uri: "fake",
+            getText(range) {
+              return command;
+            },
+          };
+
+          mcfunction_commandsCheck(doc, diagnoser);
+          diagnoser.expectAny();
+        });
+      }
+    });
+  });
+});
