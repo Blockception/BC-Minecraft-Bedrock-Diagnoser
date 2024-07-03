cf9a57ec58a184b4531c3c7cd5976876deace57c
Xterionix <72647213+Xterionix@users.noreply.github.com>
2024-07-03 16:02:48 +0500

    - Add block id check func

--- src/Lib/Diagnostics/BehaviorPack/Block/components/diagnose.ts
+++ src/Lib/Diagnostics/BehaviorPack/Block/components/diagnose.ts
@@ -5,6 +5,7 @@ import { ComponentCheck, components_check, component_error } from "../../../../U
 import { resourcepack_has_model } from "../../../ResourcePack/Model/diagnose";
 import { check_geo_and_rules } from '../../../ResourcePack/BlockCulling';
 import { behaviorpack_loot_table_diagnose } from '../../Loot Table';
+import { behaviorpack_check_blockid } from '../diagnose';
 
 /**
  *
@@ -21,10 +22,36 @@ export function behaviorpack_diagnose_block_components(
 }
 
 const component_test: Record<string, ComponentCheck> = {
-  "minecraft:creative_category": component_error(
-    "Creative category is no longer supported, replace with `description.category`, needs version 1.19.30",
-    "behaviorpack.block.components.deprecated"
-  ),
+  "minecraft:destroy_time": deprecated_component('minecraft:destructible_by_mining'),
+  "minecraft:block_light_emission": deprecated_component('minecraft:light_emission'),
+  "minecraft:block_light_absorption": deprecated_component('minecraft:light_dampening'),
+  "minecraft:block_light_filter": deprecated_component('minecraft:light_dampening'),
+  "minecraft:explosion_resistance": deprecated_component('minecraft:destructible_by_explosion'),
+  "minecraft:entity_collision": deprecated_component('minecraft:collision_box'),
+  "minecraft:block_collision": deprecated_component('minecraft:selection_box'),
+  "minecraft:pick_collision": deprecated_component('minecraft:selection_box'),
+  "minecraft:aim_collision": deprecated_component('minecraft:selection_box'),
+  "minecraft:rotation": deprecated_component('minecraft:transformation'),
+  "minecraft:breathability": deprecated_component(),
+  "minecraft:partial_visibility": deprecated_component('minecraft:geometry.bone_visibility'),
+  "minecraft:creative_category": deprecated_component('description.menu_category'),
+  "minecraft:queued_ticking": deprecated_component( 'minecraft:custom_components'),
+  "minecraft:random_ticking": deprecated_component( 'minecraft:custom_components'),
+  "minecraft:on_step_on": deprecated_component( 'minecraft:custom_components'),
+  "minecraft:on_step_off": deprecated_component( 'minecraft:custom_components'),
+  "minecraft:on_player_destroyed": deprecated_component('minecraft:custom_components'),
+  "minecraft:on_fall_on": deprecated_component('minecraft:custom_components'),
+  "minecraft:on_placed": deprecated_component('minecraft:custom_components'),
+  "minecraft:on_player_placing": deprecated_component('minecraft:custom_components'),
+  "minecraft:on_interact": deprecated_component('minecraft:custom_components'),
+  "minecraft:unit_cube": deprecated_component('geometry.minecraft:full_block'),
+  "minecraft:placement_filter": (name, component, context, diagnoser) => {
+    for (const condition of component.conditions) {
+      condition.block_filter.forEach((block: string) => {
+        behaviorpack_check_blockid(block, diagnoser)
+      });
+    }
+  },
   "minecraft:geometry": (name, component, context, diagnoser) => {
     if (typeof component === "string") {
       resourcepack_has_model(component, diagnoser);
@@ -39,3 +66,8 @@ const component_test: Record<string, ComponentCheck> = {
     if (typeof component === "string") behaviorpack_loot_table_diagnose(component, diagnoser);
   },
 };
+
+function deprecated_component(replacement?: string) {
+  let str = replacement ? ", replace with " + replacement : ''
+  return component_error("This component is no longer supported" + str + '. You are recommended to use the latest format version.', 'behaviorpack.block.components.deprecated')
+}
