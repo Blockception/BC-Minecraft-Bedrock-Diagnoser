d54891b908bbc46e7b6786d8f24f85d2627c51cb
Xterionix <72647213+Xterionix@users.noreply.github.com>
2024-07-14 14:18:48 +0500

    - Add item component diagnostics

---
+++ src/Lib/Diagnostics/BehaviorPack/Item/components/index.ts
@@ -0,0 +1,4 @@
+/*	Auto generated	*/
+
+export * from "./dependencies";
+export * from "./diagnose";
--- src/Lib/Diagnostics/BehaviorPack/Item/entry.ts
+++ src/Lib/Diagnostics/BehaviorPack/Item/entry.ts
@@ -1,12 +1,32 @@
-import { DocumentDiagnosticsBuilder } from "../../../Types";
+import { DocumentDiagnosticsBuilder, DiagnosticSeverity } from "../../../Types";
 import { diagnose_molang } from "../../Molang/diagnostics";
+import { Json } from "../../Json";
+import { Internal } from "bc-minecraft-bedrock-project";
+import { Context } from "../../../utility/components";
+import { getUsedComponents } from "bc-minecraft-bedrock-types/lib/src/minecraft/components";
+import { behaviorpack_item_components_dependencies } from "./components/dependencies";
+import { behaviorpack_diagnose_item_components } from "./components/diagnose";
 
 /**Diagnoses the given document as an item
- * @param doc The text document to diagnose
  * @param diagnoser The diagnoser builder to receive the errors*/
 export function Diagnose(diagnoser: DocumentDiagnosticsBuilder): void {
-  //Check molang
-  diagnose_molang(diagnoser.document.getText(), "Items", diagnoser);
 
-  //TODO add diagnostics
+  const item = Json.LoadReport<Internal.BehaviorPack.Item>(diagnoser);
+  if (!Internal.BehaviorPack.Item.is(item)) return;
+
+  //Check components
+  const context: Context = {
+    components: getUsedComponents(item["minecraft:item"]),
+  };
+
+  behaviorpack_diagnose_item_components(item["minecraft:item"], context, diagnoser);
+
+  behaviorpack_item_components_dependencies(item, context, diagnoser);
+
+  if (item["minecraft:item"]["events"]) diagnoser.add(
+    `events`,
+    `Item events have been deprecated in favour of \`minecraft:custom_components\`.`,
+    DiagnosticSeverity.error,
+    "behaviorpack.item.deprecated"
+  );
 }
