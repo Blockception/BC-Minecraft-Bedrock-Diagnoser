6af037be8c99449f0ba59b7a1d82a1500ba5e64f
DaanV2 <daanverstraten@hotmail.com>
2024-08-11 14:18:19 +0200

    Optimizing languages

--- src/Lib/Diagnostics/Minecraft/Language.ts
+++ src/Lib/Diagnostics/Minecraft/Language.ts
@@ -2,7 +2,7 @@ import { DiagnosticsBuilder, DiagnosticSeverity, DocumentDiagnosticsBuilder } fr
 import { Types } from "bc-minecraft-bedrock-types";
 
 export function minecraft_language_diagnose(diagnoser: DocumentDiagnosticsBuilder): void {
-  const keys: Types.OffsetWord[] = [];
+  const keys = new Map<string, number>();
   let lastOffset = 0;
   const text = diagnoser.document.getText();
   const lines = text.split("\n");
@@ -27,24 +27,24 @@ export function minecraft_language_diagnose(diagnoser: DocumentDiagnosticsBuilde
  */
 export function minecraft_language_line_diagnose(
   line: Types.OffsetWord,
-  keys: Types.OffsetWord[],
+  keys: Map<string, number>,
   diagnoser: DiagnosticsBuilder
 ): void {
   //Find comment on line
   let text = line.text;
-  const CommentIndex = text.indexOf("#");
+  const commandIndex = text.indexOf("#");
 
   //If comment has been found
-  if (CommentIndex >= 0) {
+  if (commandIndex >= 0) {
     //Ensuring that comment has started right '##'
-    if (text.substring(CommentIndex, CommentIndex + 2) !== "##") {
+    if (text.substring(commandIndex, commandIndex + 2) !== "##") {
       diagnoser.add(line, "A comment is always ##", DiagnosticSeverity.error, "minecraft.language.comment.invalid");
     }
 
     //Check if the comment doesn't start at the start of the line
-    if (CommentIndex > 0) {
+    if (commandIndex > 0) {
       //Comments need to be predicated with a tab if they do not start at the beginning of the line
-      if (text.charAt(CommentIndex - 1) !== "\t") {
+      if (text.charAt(commandIndex - 1) !== "\t") {
         diagnoser.add(
           line,
           "Before a comment must be a tab",
@@ -55,13 +55,13 @@ export function minecraft_language_line_diagnose(
     }
 
     //Remove comment;
-    text = text.substring(0, CommentIndex).trim();
+    text = text.substring(0, commandIndex).trim();
   }
 
   //If line is empty
   if (text === "" || text === "\r" || text === "\r\n" || text == "") {
     //If the line was an identend comment, it will leave an empty line
-    if (CommentIndex > 0) {
+    if (commandIndex > 0) {
       diagnoser.add(
         line,
         "A line cannot have an indented comment",
@@ -74,10 +74,10 @@ export function minecraft_language_line_diagnose(
   }
 
   //Find end of key
-  const Index = text.indexOf("=");
+  const assignIndex = text.indexOf("=");
 
   //If no key definition has been found, it means an invalid line has been given
-  if (Index < 0) {
+  if (assignIndex < 0) {
     diagnoser.add(
       line,
       "A translation item needs a '=' to separate key and value",
@@ -85,22 +85,20 @@ export function minecraft_language_line_diagnose(
       "minecraft.language.separator"
     );
   } else {
-    const Key = text.substring(0, Index);
-    const KeyIndex = keys.findIndex((item) => item.text == Key);
+    const key = text.substring(0, assignIndex);
+    const existingKey = keys.get(key);
 
     //If the key is found in the existing list of keys, then produce an error
-    if (KeyIndex >= 0) {
-      const secondKey = keys[KeyIndex];
-
+    if (existingKey) {
       diagnoser.add(line, "Duplicate key found", DiagnosticSeverity.error, "minecraft.language.duplicate");
-      diagnoser.add(secondKey, "Duplicate key found", DiagnosticSeverity.error, "minecraft.language.duplicate");
+      diagnoser.add(existingKey, "Duplicate key found", DiagnosticSeverity.error, "minecraft.language.duplicate");
+    } else {
+      keys.set(key, line.offset);
     }
 
-    keys.push(Types.OffsetWord.create(Key, line.offset));
+    const value = text.substring(assignIndex + 1);
 
-    const value = text.substring(Index + 1);
-
-    let offset = line.offset + Index + 1;
+    let offset = line.offset + assignIndex + 1;
     let index;
     if ((index = value.indexOf("\\n")) > -1)
       diagnoser.add(
@@ -126,7 +124,7 @@ export function minecraft_language_line_diagnose(
   }
 
   //The value needs to be something
-  if (Index >= text.length) {
+  if (assignIndex >= text.length) {
     diagnoser.add(
       line,
       "A value must be at least length of 1 or more",
