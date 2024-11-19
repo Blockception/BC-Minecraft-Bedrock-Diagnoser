d1466e41c9c7e9be80a11232717e80da2c6ed017
Xterionix <72647213+Xterionix@users.noreply.github.com>
2024-11-19 13:09:00 +0500

    Account for block references being objects of format {"name": "value"} (#280)
    * - Account for block references being objects of format {"name": "value"}
    
    * Update src/Lib/Diagnostics/BehaviorPack/Block/components/diagnose.ts
    
    Co-authored-by: Daan Verstraten <daanverstraten@hotmail.com>
    
    * adding simplification
    
    ---------
    
    Co-authored-by: Daan Verstraten <daanverstraten@hotmail.com>

--- src/Lib/Diagnostics/BehaviorPack/Item/components/diagnose.ts
+++ src/Lib/Diagnostics/BehaviorPack/Item/components/diagnose.ts
@@ -47,21 +47,27 @@ const component_test: Record<string, ComponentCheck> = {
   saddle: deprecated_component(),
   "minecraft:entity_placer": (name, component, context, diagnoser) => {
     if (Array.isArray(component.dispense_on))
-      component.dispense_on.forEach((block: string) => {
-        behaviorpack_check_blockid(block, diagnoser);
+      component.dispense_on.forEach((block: string | { name: string }) => {
+        if (typeof block == 'object' && 'name' in block) behaviorpack_check_blockid(block.name, diagnoser)
+          else if (typeof block == 'string') behaviorpack_check_blockid(block, diagnoser);
       });
     if (Array.isArray(component.use_on))
-      component.use_on.forEach((block: string) => {
-        behaviorpack_check_blockid(block, diagnoser);
+      component.use_on.forEach((block: string | { name: string }) => {
+        if (typeof block == 'object' && 'name' in block) behaviorpack_check_blockid(block.name, diagnoser)
+          else if (typeof block == 'string') behaviorpack_check_blockid(block, diagnoser);
       });
     if (component.entity) behaviorpack_entityid_diagnose(component.entity, diagnoser);
   },
   "minecraft:block_placer": (name, component, context, diagnoser) => {
     if (Array.isArray(component.use_on))
-      component.use_on.forEach((block: string) => {
-        behaviorpack_check_blockid(block, diagnoser);
+      component.use_on.forEach((block: string | { name: string }) => {
+        if (typeof block == 'object' && 'name' in block) behaviorpack_check_blockid(block.name, diagnoser)
+          else if (typeof block == 'string') behaviorpack_check_blockid(block, diagnoser);
       });
-    if (component.block) behaviorpack_check_blockid(component.block, diagnoser);
+    if (component.block) {
+      if (typeof component.block == 'object' && 'name' in component.block) behaviorpack_check_blockid((component.block as { name: string }).name, diagnoser)
+        else if (typeof component.block == 'string') behaviorpack_check_blockid(component.block, diagnoser);
+    }
   },
   "minecraft:projectile": (name, component, context, diagnoser) => {
     if (component.projectile_entity) behaviorpack_entityid_diagnose(component.projectile_entity, diagnoser);
