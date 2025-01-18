721d44cb310a6bbf32143d6d3e40220c876cb640
DaanV2 <daanverstraten@hotmail.com>
2025-01-18 20:35:03 +0100

    chore: updating dependencies

--- src/Lib/Diagnostics/Minecraft/Effect.ts
+++ src/Lib/Diagnostics/Minecraft/Effect.ts
@@ -5,6 +5,5 @@ import { Types } from "bc-minecraft-bedrock-types";
 export function minecraft_effect_diagnose(value: Types.OffsetWord, diagnoser: DiagnosticsBuilder): void {
   //Check if minecraft has effect data
   if (MinecraftData.General.Effects.includes(value.text)) return;
-
   diagnoser.add(value, "Effect does not exist: " + value.text, DiagnosticSeverity.error, "minecraft.effect.invalid");
 }
