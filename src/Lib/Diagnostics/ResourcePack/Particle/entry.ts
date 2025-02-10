3222a60d0107ff28fa61eebe310ea5341b079fa7
Xterionix <72647213+Xterionix@users.noreply.github.com>
2025-02-10 21:28:14 +0500

    Add texture validation for particles

--- src/Lib/Diagnostics/ResourcePack/Particle/entry.ts
+++ src/Lib/Diagnostics/ResourcePack/Particle/entry.ts
@@ -1,5 +1,9 @@
-import { DocumentDiagnosticsBuilder } from "../../../Types";
+import { Internal } from 'bc-minecraft-bedrock-project';
+import { DiagnosticSeverity, DocumentDiagnosticsBuilder } from "../../../Types";
+import { Json } from '../../Json';
 import { diagnose_molang } from '../../Molang/diagnostics';
+import { MinecraftData } from 'bc-minecraft-bedrock-vanilla-data';
+import { education_enabled } from '../../Definitions';
 
 /**Diagnoses the given document as a particle
  * @param doc The text document to diagnose
@@ -8,5 +12,28 @@ export function Diagnose(diagnoser: DocumentDiagnosticsBuilder): void {
   //Check molang
   diagnose_molang(diagnoser.document.getText(), "Particles", diagnoser);
 
-  //TODO add rp diagnostics
+  const particle = Json.LoadReport<Internal.ResourcePack.Particle>(diagnoser);
+  if (!Internal.ResourcePack.Particle.is(particle)) return;
+
+  //@ts-ignore
+  const texture = particle.particle_effect?.description?.['basic_render_parameters']?.['texture'];
+
+  if (typeof texture != 'string') return;
+
+  const pack = diagnoser.context.getCache().resourcePacks.get(diagnoser.document.uri);
+  if (pack === undefined) return;
+
+    if (MinecraftData.ResourcePack.hasTexture(texture, education_enabled(diagnoser))) return;
+
+  const rp_files = diagnoser.context
+    .getFiles(pack.folder, ["**/textures/**/*.{tga,png,jpg,jpeg}"], pack.context.ignores)
+    .map((item) => item.replace(/\\/gi, "/"));
+
+  if (!rp_files.some(path => path.includes(texture))) diagnoser.add(
+    `${texture}`,
+    `Cannot find file: ${texture}`,
+    DiagnosticSeverity.error,
+    "resourcepack.texture.missing"
+  );
+  
 }
