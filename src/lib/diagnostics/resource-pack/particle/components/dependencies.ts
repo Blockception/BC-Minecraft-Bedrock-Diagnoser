555d947688680836af626cb64257dd2934f0d0b6
Xterionix <xterionyt@gmail.com>
2025-05-18 11:06:36 +0500

    - appearance_billboard max_frame check

---
+++ src/lib/diagnostics/resource-pack/particle/components/dependencies.ts
@@ -0,0 +1,23 @@
+import { Internal } from "bc-minecraft-bedrock-project";
+import { DiagnosticsBuilder } from '../../../../types';
+import { components_dependencies, Context, DependedMap } from '../../../../utility/components';
+
+//Map of components that are depended on all other specified components
+const component_dependents_all: DependedMap = {
+};
+
+//Map of components that are depended on one of the other specified components
+const component_dependents_any: DependedMap = {
+};
+
+/**
+ * Checks if components dependencies are present, a component might need others to be present
+ * @param particle The entity to check
+ * @param diagnoser The diagnoser to report to*/
+ export function resourcepack_particle_components_dependencies(
+  particle: Internal.ResourcePack.Particle,
+  context: Context<Internal.ResourcePack.Particle>,
+  diagnoser: DiagnosticsBuilder
+): void {
+  components_dependencies("particle", context, diagnoser, component_dependents_all, component_dependents_any);
+}
--- src/lib/diagnostics/resource-pack/particle/entry.ts
+++ src/lib/diagnostics/resource-pack/particle/entry.ts
@@ -5,6 +5,9 @@ import { diagnose_molang } from '../../molang/diagnostics';
 import { MinecraftData } from 'bc-minecraft-bedrock-vanilla-data';
 import { education_enabled } from '../../definitions';
 import { texture_files_diagnose } from '../texture-atlas';
+import { resourcepack_diagnose_particle_components } from './components';
+import { Context } from '../../../utility/components';
+import { getUsedComponents } from 'bc-minecraft-bedrock-types/lib/minecraft/components';
 
 /**Diagnoses the given document as a particle
  * @param doc The text document to diagnose
@@ -16,6 +19,13 @@ export function Diagnose(diagnoser: DocumentDiagnosticsBuilder): void {
   const particle = Json.LoadReport<Internal.ResourcePack.Particle>(diagnoser);
   if (!Internal.ResourcePack.Particle.is(particle)) return;
 
+  //check components
+  const context: Context<Internal.ResourcePack.Particle> = {
+    source: particle,
+    components: getUsedComponents(particle.particle_effect.components),
+  }
+  resourcepack_diagnose_particle_components(particle.particle_effect, context, diagnoser);
+
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   //@ts-ignore
   const texture = particle.particle_effect?.description?.['basic_render_parameters']?.['texture'];
@@ -25,12 +35,12 @@ export function Diagnose(diagnoser: DocumentDiagnosticsBuilder): void {
   const pack = diagnoser.context.getCache().resourcePacks.get(diagnoser.document.uri);
   if (pack === undefined) return;
 
-    if (MinecraftData.ResourcePack.hasTexture(texture, education_enabled(diagnoser))) return;
+  if (MinecraftData.ResourcePack.hasTexture(texture, education_enabled(diagnoser))) return;
 
   const rp_files = diagnoser.context
     .getFiles(pack.folder, ["**/textures/**/*.{tga,png,jpg,jpeg}"], pack.context.ignores)
     .map((item) => item.replace(/\\/gi, "/"));
 
   texture_files_diagnose('texture', texture, rp_files, diagnoser)
-  
+
 }
