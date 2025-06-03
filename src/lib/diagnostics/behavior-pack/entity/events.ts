291495512dfc151b5c44d1d55aea1f5a44120eaa
Xterionix <72647213+Xterionix@users.noreply.github.com>
2025-06-03 15:55:30 +0500

    - Add info warning for if randomize/sequence only have on entry (#372)

--- src/lib/diagnostics/behavior-pack/entity/events.ts
+++ src/lib/diagnostics/behavior-pack/entity/events.ts
@@ -45,9 +45,13 @@ export function behaviorpack_entity_check_event(
     behaviorpack_entity_check_event(item, event_id, diagnoser, properties, component_groups, eventIds);
   });
 
+  if (event.randomize?.length == 1) diagnoser.add(event_id, "Randomize only has one entry and can therefore be removed.", DiagnosticSeverity.info, "behaviorpack.entity.event.randomize.length")
+
   event.sequence?.forEach((item) => {
     behaviorpack_entity_check_event(item, event_id, diagnoser, properties, component_groups, eventIds);
   });
+
+  if (event.sequence?.length == 1) diagnoser.add(event_id, "Sequence only has one entry and can therefore be removed.", DiagnosticSeverity.info, "behaviorpack.entity.event.sequence.length")
 
   behaviorpack_entity_components_filters(event, diagnoser);
 
