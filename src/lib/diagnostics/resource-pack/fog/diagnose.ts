91ad859fbbcdf3e825aaf5120a01753c36ef6780
Xterionix <xterionyt@gmail.com>
2025-06-10 22:17:30 +0500

    - Add fog_is_defined

---
+++ src/lib/diagnostics/resource-pack/fog/diagnose.ts
@@ -0,0 +1,13 @@
+import { Types } from "bc-minecraft-bedrock-types";
+import { Errors } from '../..';
+import { DiagnosticsBuilder } from "../../../../main";
+
+export function fog_is_defined(id: string, diagnoser: DiagnosticsBuilder): boolean {
+  //Project has fog
+  const fog = diagnoser.context.getProjectData().resources.fogs.get(id, diagnoser.project);
+  if (fog === undefined) {
+    Errors.missing("resources", "fogs", id, diagnoser);
+    return false;
+  }
+  return true;
+}
