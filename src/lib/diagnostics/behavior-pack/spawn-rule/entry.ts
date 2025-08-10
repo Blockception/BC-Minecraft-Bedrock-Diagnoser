36b19a5837ff789d9dbefa60f82e45517a616dfb
Xterionix <72647213+Xterionix@users.noreply.github.com>
2025-08-10 13:58:22 +0500

    - Add spawn rule checking (#397)
    - Update bc-minecraft-bedrock-project to latest

--- src/lib/diagnostics/behavior-pack/spawn-rule/entry.ts
+++ src/lib/diagnostics/behavior-pack/spawn-rule/entry.ts
@@ -1,5 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Internal } from 'bc-minecraft-bedrock-project';
+import { Json } from '../../json';
 import { DocumentDiagnosticsBuilder } from "../../../types";
+import { behaviorpack_entityid_diagnose } from '../entity';
+import { Context } from "../../../utility/components";
+import { behaviorpack_diagnose_spawnrule_components } from './components';
 
 /**
  * Diagnoses the given document as an spawn rule
@@ -7,5 +11,24 @@ import { DocumentDiagnosticsBuilder } from "../../../types";
  * @param diagnoser The diagnoser builder to receive the errors
  */
 export function diagnose_spawn_rule_document(diagnoser: DocumentDiagnosticsBuilder): void {
-  //TODO add diagnostics
+  const rule = Json.LoadReport<Internal.BehaviorPack.SpawnRule>(diagnoser);
+  if (!Internal.BehaviorPack.SpawnRule.is(rule)) return;
+
+  behaviorpack_entityid_diagnose(rule['minecraft:spawn_rules'].description.identifier, diagnoser);
+
+  rule['minecraft:spawn_rules'].conditions.forEach(data => {
+    const out: string[] = [];
+
+    Object.getOwnPropertyNames(data).forEach((c) => {
+      if (!out.includes(c)) out.push(c);
+    });
+
+    const context: Context<Internal.BehaviorPack.SpawnRule> = {
+      source: rule,
+      components: out,
+    };
+
+    behaviorpack_diagnose_spawnrule_components(data, context, diagnoser);
+  })
+
 }
